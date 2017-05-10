e832fa605cebc255e97e99c4a47129052c00aaa3
Gabriel <gabrielm.jaeger@yahoo.de>
2017-05-10 11:26:54 +0800

    images resized/updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,28 @@ $(document).ready(function() {
   $('.drone').click(function(){
     $('#video').modal("show");
   });
+
+  $('#video').on('hidden.bs.modal', function(){
+    $iframe = $(this).find("iframe");
+    $iframe.attr("src", $iframe.attr("src"));
+  });
+
+  $(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.image-title').each( function(i){
+
+        var bottom_of_object = $(this).offset().top + ($(this).outerHeight());
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object){
+
+            $(this).animate({'opacity':'0.5'},200);
+
+        }
+
+    });
+
+});
 });
